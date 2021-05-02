20979b1cc731c33528f373cb8b8cc30582719203
l0b030a <lokesh.bhoyar@walmartlabs.com>
2021-05-02 18:21:35 +0530

    nf

--- jsontext/Readme.md
+++
@@ -1,3 +0,0 @@
-# JSON Text
-
-Beautify json text that displays on browser from API's with a single click on extension icon.
--- jsontext/index.js
+++ jsontext/index.js
@@ -1,45 +1,54 @@
 function jsonBeautify(){
-	var data = document.documentElement.innerText.replace(/ /g, '');
-	var tabs = 0;
-	var modified = "";
-	var span = false;
-	var stringStart = false;
-	for (i in data){
-		var c = data.charAt(i);
-		if( c == '{' || c == '['){
-			tabs ++;
-			modified += "<span style='color:grey;'>" + c + "</span>" + "\n" + "   ".repeat(tabs);
-		}
-		else if(c == '}' || c == ']'){
-			tabs --;
-			if(span){
-				modified += "</span>" + "\n" + "   ".repeat(tabs) + "<span style='color:grey;'>" + c + "</span>";
-				span = false;
-			} else {
-				modified += "\n" + "   ".repeat(tabs) + "<span style='color:grey;'>" + c + "</span>";
-			}
-		}
-		else if(c == ','){
-			if(span){
-				modified += "</span>"+ c + "\n" + "   ".repeat(tabs);
-				span = false;
-			}else{
-				modified += c + "\n" + "   ".repeat(tabs);
-			}
-		}
-		else if(c == ':' && !stringStart){
-			if(data.charAt(parseInt(i)+1) == '[' || data.charAt(parseInt(i)+1) == '{'){
-				modified += c;
-			}else{
-				modified += c + "<span style='color:green;'> ";
-				span = true;
-			}
+	let data = document.documentElement.innerText.replace(/ /g, '');
+	let tabs = 0;
+	let modified = "";
+	let span = false;
+	let stringStart = false;
 
-		}else if(c == '"'){
+	for (let i in data){
+		let c = data.charAt(i);
+
+		console.log(c);
+		if(c === '"'){
 			stringStart = !stringStart;
 			modified += c;
-		}
-		else{
+		} else if(!stringStart){
+			if( c === '{' || c === '['){
+				tabs ++;
+				modified += "<span style='color:grey;'>" + c + "</span>" + "\n" + "   ".repeat(tabs);
+			}
+			else if(c === '}' || c === ']'){
+				tabs --;
+				if(span){
+					modified += "</span>" + "\n" + "   ".repeat(tabs) + "<span style='color:grey;'>" + c + "</span>";
+					span = false;
+				} else {
+					modified += "\n" + "   ".repeat(tabs) + "<span style='color:grey;'>" + c + "</span>";
+				}
+			}
+			else if(c === ','){
+				if(span){
+					modified += "</span>"+ c + "\n" + "   ".repeat(tabs);
+					span = false;
+				}else{
+					modified += c + "\n" + "   ".repeat(tabs);
+				}
+			}
+			else if(c === ':'){
+				if(data.charAt(parseInt(i)+1) === '[' || data.charAt(parseInt(i)+1) === '{'){
+					modified += c;
+				}else if(data.charAt(parseInt(i)+1) === '"'){
+					modified += c + "<span style='color:green;'> ";
+					span = true;
+				}else{
+					modified += c + "<span style='color:blue;'> ";
+					span = true;
+				}
+
+			}else {
+				modified += c;
+			}
+		} else{
 			modified += c;
 		}
 	}
